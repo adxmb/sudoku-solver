842d349a443cbdb630784af82e653344a67c666c
Gluglug7 <abod278@aucklanduni.ac.nz>
2024-02-12 18:49:21 +1300

    Updating legality checking functions

--- solver.js
+++ solver.js
@@ -12,6 +12,7 @@ let board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+// Reads the input board and updates the board array
 function readBoard() {
   for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE; j++) {
@@ -22,13 +23,6 @@ function readBoard() {
         cell.style.backgroundColor = "#c5eded";
       } else {
         board[i][j] = parseInt(cell.value);
-        if (
-          checkRow(i, board[i][j]) ||
-          checkCol(j, board[i][j]) ||
-          checkSquare(i, j, board[i][j])
-        ) {
-          return false;
-        }
         cell.style.backgroundColor = "#fefcbf";
       }
       cell.setAttribute("disabled", "true");
@@ -37,6 +31,7 @@ function readBoard() {
   return true;
 }
 
+// Updates the input cells with the board values
 function setBoard() {
   for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE; j++) {
@@ -46,6 +41,7 @@ function setBoard() {
   }
 }
 
+// Clears the board and enables all cells
 function clearBoard() {
   for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE; j++) {
@@ -57,43 +53,33 @@ function clearBoard() {
   }
 }
 
+// Checks if a number is already in the row, column or 3x3 box
+function isLegal(row, col, num) {
+  return checkRow(row, num) && checkCol(col, num) && checkBox(row, col, num);
+}
+
+// Helper function for isLegal
 function checkRow(row, num) {
   for (let i = 0; i < SIZE; i++) {
     if (board[row][i] === num) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function checkCol(col, num) {
-  for (let i = 0; i < SIZE; i++) {
-    if (board[i][col] === num) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function checkSquare(row, col, num) {
-  let startRow = row - (row % 3);
-  let startCol = col - (col % 3);
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (board[i + startRow][j + startCol] === num) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function isLegal(row, col, num) {
-  for (let i = 0; i < SIZE; i++) {
-    if (board[row][i] === num || board[i][col] === num) {
       return false;
     }
   }
+  return true;
+}
+
+// Helper function for isLegal
+function checkCol(col, num) {
+  for (let i = 0; i < SIZE; i++) {
+    if (board[i][col] === num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Helper function for isLegal
+function checkBox(row, col, num) {
   let startRow = row - (row % 3);
   let startCol = col - (col % 3);
   for (let i = 0; i < 3; i++) {
